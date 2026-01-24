8646057dbea0ff764296ff8e4ef1c806265e5bc9
Luis Lopez <lopezluisarmando034@gmail.com>
2026-01-24 18:21:36 -0500

    Implementación correcta de fcfs sjf rr

--- src/schdl/fcfs.c
+++ src/schdl/fcfs.c
@@ -6,9 +6,42 @@
  * ============================================================ */
 void fcfs_schedule(Process p[], int n)
 {
-    (void)p;
-    (void)n;
-    /* TODO: Implement FCFS scheduling algorithm here */
+    int current_time = 0;
+    int completed = 0;
+
+    while (completed < n)
+    {
+        int idx = -1;
+        int min_arrival = 999999;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!p[i].completed)
+            {
+                if (p[i].arrival_time < min_arrival)
+                {
+                    min_arrival = p[i].arrival_time;
+                    idx = i;
+                }
+                else if (p[i].arrival_time == min_arrival &&
+                         p[i].id < p[idx].id)
+                {
+                    idx = i;
+                }
+            }
+        }
+
+        if (current_time < p[idx].arrival_time)
+            current_time = p[idx].arrival_time;
+
+        p[idx].waiting_time = current_time - p[idx].arrival_time;
+        p[idx].turnaround_time = p[idx].waiting_time + p[idx].burst_time;
+
+        current_time += p[idx].burst_time;
+
+        p[idx].completed = 1;
+        completed++;
+    }
 }
 
 /* ============================================================
